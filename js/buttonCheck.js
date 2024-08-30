845584205e35bcb8bc8c85c9cb82b072a66cfcc8
quiz-bosch <vanessadantas271@gmail.com>
2024-08-30 06:44:51 -0300

    Update buttonCheck.js

--- js/buttonCheck.js
+++ js/buttonCheck.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Seletores de botões de grau e resposta
     let cardGradeButtons = document.querySelectorAll('.card__grade');
     let resposta1Buttons = document.querySelectorAll('.resposta1');
     let resposta2Buttons = document.querySelectorAll('.resposta2');
 
+    // Adiciona eventos de clique para os botões de grau
     cardGradeButtons.forEach(function (button) {
         button.addEventListener('click', function () {
             toggleButton(this, cardGradeButtons);
@@ -10,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // Adiciona eventos de clique para os botões de resposta1
     resposta1Buttons.forEach(function (button) {
         button.addEventListener('click', function () {
             toggleButton(this, resposta1Buttons);
@@ -17,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // Adiciona eventos de clique para os botões de resposta2
     resposta2Buttons.forEach(function (button) {
         button.addEventListener('click', function () {
             toggleButton(this, resposta2Buttons);
@@ -33,11 +37,15 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function checkConditions() {
-        const enviarRespostaButton = document.getElementById('enviarResposta');
-
+        // Encontre o botão "Enviar Resposta" mais próximo do elemento ativo
         const cardGradeActive = document.querySelector('.card__grade.active');
         const resposta1Active = document.querySelector('.resposta1.active');
         const resposta2Active = document.querySelector('.resposta2.active');
+
+        let enviarRespostaButton;
+        if (cardGradeActive) {
+            enviarRespostaButton = cardGradeActive.closest('.card').querySelector('.card__enviarResposta');
+        }
 
         if (enviarRespostaButton && cardGradeActive && resposta1Active && resposta2Active) {
             // Ativa o botão se todas as condições são atendidas
@@ -45,7 +53,10 @@ document.addEventListener('DOMContentLoaded', function () {
             enviarRespostaButton.classList.add('active');
         } else {
             // Desativa o botão se alguma condição não for atendida
-            enviarRespostaButton.setAttribute('disabled', 'true');
+            if (enviarRespostaButton) {
+                enviarRespostaButton.setAttribute('disabled', 'true');
+                enviarRespostaButton.classList.remove('active');
+            }
         }
     }
 });
